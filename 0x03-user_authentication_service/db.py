16da150c43805fd19f30f9c5e2cfe56ae1faf56e
astianmuchui <astianmuchui@gmail.com>
2024-04-25 13:12:28 +0300

    CRUD

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 from typing import string
 
@@ -31,10 +31,33 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: string, hashed_password: string) -> User:
-        """Add a new user
-        """
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Filters users by parameters passed"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound as e:
+            self._session.rollback()
+            raise e
+        except InvalidRequestError as e:
+            self._session.rollback()
+            raise e
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates user data"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
