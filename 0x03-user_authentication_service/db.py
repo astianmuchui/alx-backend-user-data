451e1b0ef315ce5aa8835fdc663e61c447966420
astianmuchui <astianmuchui@gmail.com>
2024-04-25 13:16:33 +0300

    CRUD

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import InvalidRequestError
 from user import Base, User
 from typing import string
 
