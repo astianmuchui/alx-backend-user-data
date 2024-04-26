ed151ef0de866e7aed4cbb27f09bdb741727c809
astianmuchui <astianmuchui@gmail.com>
2024-04-26 15:45:05 +0300

    Minor fixed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,7 +18,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(250), nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250), nullable=True)
-    reset_token = Column(VARCHAR(250), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
