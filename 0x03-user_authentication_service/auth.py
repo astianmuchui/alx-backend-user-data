69e40e5123a0e7d29561a0d6f6ecbfcad8650051
astianmuchui <astianmuchui@gmail.com>
2024-04-26 17:27:15 +0300

    Added Type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> None:
+    def register_user(self, email: str, password: str) -> User:
         """
         Registers a new user
         """
